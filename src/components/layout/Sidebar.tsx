9cf664813321931a9bb99c4800ea91ed47472737
EIUKLTD <moltrasio@eurotron-uk.com>
2026-07-19 19:48:38 +0100

    Update Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { createClient } from '@/lib/supabase'
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation' 
 import { useState } from 'react'
 import clsx from 'clsx'
 
